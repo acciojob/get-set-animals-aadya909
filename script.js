55c5ff39a46469f295156bc64188d2ffa71d85a7
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-03-27 21:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-//complete this code
 class Animal {
-	constructor(species :string )
-	this.species=species;
+  private _species: string;
+
+  constructor(species: string) {
+    this._species = species;
+  }
+
+  get species(): string {
+    return this._species;
+  }
+
+  set species(value: string) {
+    this._species = value;
+  }
+
+  makeSound(): void {
+    console.log(`The ${this._species} makes a sound`);
+  }
 }
 
-makeSound():void{
-	console.log(`This ${this.species} makes a sound`);
-}
 class Dog extends Animal {
-	bark():void{
-		console.log("woof");
-	}
+  bark(): void {
+    console.log("woof");
+  }
 }
 
 class Cat extends Animal {
-	purr():void{
-		console.log("purr")
-	}
+  purr(): void {
+    console.log("purr");
+  }
 }
 
 // Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+(window as any).Animal = Animal;
+(window as any).Dog = Dog;
+(window as any).Cat = Cat;
+
